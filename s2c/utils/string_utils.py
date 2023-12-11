486b4bb3c4038b84b59059daac267a206b024cb3
Rafael Tonholo <rafael@tonholo.dev>
2023-12-11 13:40:19 -0400

    chore: add string utils

---
+++ s2c/utils/string_utils.py
@@ -0,0 +1,20 @@
+# Import the 'sub' function from the 're' module for regular expression substitution
+from re import sub
+
+# Define a function to convert a string to camel case
+def camel_case(s):
+    # Use regular expression substitution to replace underscores and hyphens with spaces,
+    # then title case the string (capitalize the first letter of each word), and remove spaces
+    s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
+    
+    # Join the string, ensuring the first letter is lowercase
+    return ''.join([s[0].lower(), s[1:]])
+
+# Define a function to convert a string to pascal case
+def pascal_case(s):
+    # Use regular expression substitution to replace underscores and hyphens with spaces,
+    # then title case the string (capitalize the first letter of each word), and remove spaces
+    s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
+    
+    # Join the string, ensuring the first letter is lowercase
+    return ''.join([s[0].upper(), s[1:]])
