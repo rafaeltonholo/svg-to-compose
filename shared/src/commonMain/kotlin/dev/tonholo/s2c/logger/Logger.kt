e8eeda9f6aba97177c8ec72156053b9567ec5843
Rafael Tonholo <rafael@tonholo.dev>
2023-12-20 15:46:42 -0400

    feat: add logger

---
+++ shared/src/commonMain/kotlin/dev/tonholo/s2c/logger/Logger.kt
@@ -0,0 +1,38 @@
+package dev.tonholo.s2c.logger
+
+import AppConfig
+
+fun debug(message: String) {
+    if (AppConfig.debug) {
+        println(message)
+    }
+}
+
+fun debugSection(message: String) {
+    if (AppConfig.debug) {
+        println()
+        val section = "=".repeat(25 - (message.length / 2))
+        println("$section $message $section")
+        println()
+    }
+}
+
+fun debugEndSection() {
+    if (AppConfig.debug) {
+        println()
+        println("=".repeat(50))
+        println()
+    }
+}
+
+fun verbose(message: String) {
+    if (AppConfig.verbose) {
+        println(message)
+    }
+}
+
+fun output(message: String) {
+    println(message)
+}
+
+fun printEmpty() = println()
