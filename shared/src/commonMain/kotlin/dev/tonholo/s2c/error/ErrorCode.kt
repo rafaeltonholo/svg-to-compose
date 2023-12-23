c9221fccbe080d6508ce174aae0b179c28321b87
Rafael Tonholo <rafael@tonholo.dev>
2023-12-23 18:40:25 -0400

    feat: add optimization

--- shared/src/commonMain/kotlin/dev/tonholo/s2c/error/ErrorCode.kt
+++ shared/src/commonMain/kotlin/dev/tonholo/s2c/error/ErrorCode.kt
@@ -3,7 +3,7 @@ package dev.tonholo.s2c.error
 enum class ErrorCode(val code: Int) {
     FileNotFoundError(code = 1),
     SvgoOptimizationError(code = 2),
-    Sv2OptimizationError(code = 3),
+    S2vOptimizationError(code = 3),
     AvocadoOptimizationError(code = 4),
     NotSupportedFileError(code = 8),
     OutputNotDirectoryError(code = 9),
