32d833788c84faf7aae8b6e9b418b386213d1bad
Rafael Tonholo <rafael@tonholo.dev>
2024-02-17 15:34:28 -0400

    chore: merge FileMetadata file with Path.extension.kt

--- shared/src/commonMain/kotlin/dev/tonholo/s2c/extensions/Path.extension.kt
+++ shared/src/commonMain/kotlin/dev/tonholo/s2c/extensions/Path.extension.kt
@@ -1,6 +1,14 @@
 package dev.tonholo.s2c.extensions
 
+import okio.FileSystem
 import okio.Path
+
+val Path.isDirectory: Boolean
+    get() {
+        val metadata = FileSystem.SYSTEM.metadataOrNull(this)
+        return metadata != null && metadata.isDirectory ||
+                name.matches(".*\\.[a-zA-Z0-9]*$".toRegex()).not()
+    }
 
 val Path.extension: String
     get() = segments
