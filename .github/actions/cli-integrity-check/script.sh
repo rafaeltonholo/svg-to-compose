f239ecaafe0681c0d2522235ae3db5f1681a18ef
Rafael Tonholo <rafael@tonholo.dev>
2024-03-19 19:23:50 -0300

    ci(cli-integrity-check): add script to verify generated icons

---
+++ .github/actions/cli-integrity-check/script.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+declare -a files=(
+  "shield-halved-solid"
+  "illustration"
+)
+declare -a names=(
+  "ShieldSolid"
+  "Illustration"
+)
+root_directory=$1
+if [ "$root_directory" == "" ]; then
+  echo "The root directory must be the first parameter."
+  exit 1
+fi
+ext=$2
+if [ "$ext" == "" ]; then
+  echo "The file extension must be the second parameter."
+  exit 1
+fi
+optimize="false"
+suffix="NonOptimized"
+if [ "$3" == "optimize" ]; then
+  optimize="true"
+  suffix="Optimized"
+fi
+
+type="svg"
+if [ "$ext" == "xml" ]; then
+  type="avg"
+else
+  ext="svg"
+fi
+
+base_package="dev.tonholo.sampleApp.ui"
+package="$base_package.icon.${type}"
+theme="$base_package.theme.SampleAppTheme"
+errors=()
+
+for index in "${!files[@]}"; do
+  filename=${files[index]}
+  output_name="${names[index]}.${type}"
+  input="$root_directory/samples/${type}/$filename.${ext}"
+  output="$root_directory/integrity-check/${type}/$output_name.${suffix}.kt"
+
+  echo "Parsing $filename to Jetpack Compose Icon"
+  if ! sh "$root_directory/s2c" -o "$output" \
+        -p "$package" \
+        --theme "$theme" \
+        -opt=$optimize \
+        "$input"; then
+      echo "Failed to execute CLI integrity check."
+      exit 1
+  fi
+  diff_suffix=$(echo $suffix | tr "[:upper:]" "[:lower:]")
+  diff_file="$root_directory/samples/$output_name.${diff_suffix}.kt"
+
+  echo "Verifying $filename with provided sample."
+  if ! diff --strip-trailing-cr "$output" "$diff_file"; then
+    errors+=("$filename")
+  fi
+done
+
+echo
+if [ "${#errors[@]}" == 0 ]; then
+  echo "✅ Integrity check pass"
+else
+  echo "❌ Integrity check failed"
+  echo
+  echo "Failed files:"
+  for index in "${!errors[@]}"; do
+    echo "    - ${errors[index]}.${ext}"
+  done
+  exit 1
+fi
