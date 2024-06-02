75cf9603ac41a799160865f4acd09b0958780e62
Rafael Tonholo <rafael@tonholo.dev>
2024-06-02 09:58:58 -0300

    fix(lint): split method in smaller methods to decrease complexity docs: improve ktdocs

--- svg-to-compose/src/commonMain/kotlin/dev/tonholo/s2c/domain/IconFileContents.kt
+++ svg-to-compose/src/commonMain/kotlin/dev/tonholo/s2c/domain/IconFileContents.kt
@@ -34,6 +34,23 @@ val materialReceiverTypeImport = setOf(
     "androidx.compose.material.icons.Icons"
 )
 
+/**
+ * Represents the contents of an icon file, including its package, icon name, theme, dimensions, nodes, and other attributes.
+ *
+ * @property pkg The package name of the icon.
+ * @property iconName The name of the icon.
+ * @property theme The theme of the Compose application.
+ * @property width The width of the icon in density-independent pixels.
+ * @property height The height of the icon in density-independent pixels.
+ * @property viewportWidth The width of the viewport in floating-point units.
+ * @property viewportHeight The height of the viewport in floating-point units.
+ * @property nodes The list of nodes that make up the icon.
+ * @property receiverType The type of the receiver object for the icon, if any.
+ * @property addToMaterial Whether to add the icon to the `Icons.Filled` object.
+ * @property noPreview Whether to disable the preview for the icon.
+ * @property makeInternal Whether to make the icon internal.
+ * @property imports The set of imports to include in the generated code.
+ */
 data class IconFileContents(
     val pkg: String,
     val iconName: String,
@@ -49,6 +66,11 @@ data class IconFileContents(
     val makeInternal: Boolean = false,
     val imports: Set<String> = defaultImports,
 ) {
+    /**
+     * Generates the Kotlin code for the icon.
+     *
+     * @return The generated Kotlin code.
+     */
     fun materialize(): String = verboseSection("Generating file") {
         verbose(
             """Parameters:
@@ -66,27 +88,9 @@ data class IconFileContents(
             """.trimMargin()
         )
 
-        val iconPropertyName = when {
-            receiverType?.isNotEmpty() == true -> {
-                // as we add the dot in the next line, remove it in case the user adds a leftover dot
-                // to avoid compile issues.
-                receiverType.removeSuffix(".")
-                "$receiverType.${iconName.pascalCase()}"
-            }
-
-            addToMaterial -> "Icons.Filled.${iconName.pascalCase()}"
-            else -> iconName.pascalCase()
-        }
-
+        val iconPropertyName = buildIconPropertyName(receiverType)
         val (nodes, chunkFunctions) = chunkNodesIfNeeded()
-
-        val chunkFunctionsStr = if (!chunkFunctions.isNullOrEmpty()) {
-            """|
-               |${chunkFunctions.joinToString("\n\n") { it.createChunkFunction() }}
-               """
-        } else {
-            ""
-        }
+        val chunkFunctionsContent = buildChunkFunctionsContent(chunkFunctions)
 
         val indentSize = 12
         val pathNodes = nodes
@@ -95,41 +99,8 @@ data class IconFileContents(
                     .replace("\n", "\n${" ".repeat(indentSize)}") // fix indent
             }
 
-        val preview = if (noPreview) {
-            ""
-        } else {
-            """
-            |
-            |@Preview
-            |@Composable
-            |private fun IconPreview() {
-            |    $theme {
-            |        Column(
-            |            verticalArrangement = Arrangement.spacedBy(8.dp),
-            |            horizontalAlignment = Alignment.CenterHorizontally,
-            |        ) {
-            |            Image(
-            |                imageVector = $iconPropertyName,
-            |                contentDescription = null,
-            |                modifier = Modifier
-            |                    .width((${max(width, viewportWidth)}).dp)
-            |                    .height((${max(height, viewportHeight)}).dp),
-            |            )
-            |        }
-            |    }
-            |}
-            """
-        }
-
-        val extraContent = buildString {
-            if (chunkFunctionsStr.isNotEmpty()) {
-                appendLine(chunkFunctionsStr.trimMargin())
-            }
-            if (preview.isNotEmpty()) {
-                appendLine(preview.trimMargin())
-            }
-        }
-
+        val preview = buildPreview(iconPropertyName)
+        val extraContent = buildExtraContent(chunkFunctionsContent, preview)
         val visibilityModifier = if (makeInternal) "internal " else ""
 
         return@verboseSection """
@@ -160,6 +131,89 @@ data class IconFileContents(
             .trimMargin()
     }
 
+    /**
+     * Builds the content of the `chunkFunctions` property for the generated class.
+     *
+     * @param chunkFunctions The list of `ChunkFunction` objects.
+     * @return The content of the `chunkFunctions` property as a string.
+     */
+    private fun buildChunkFunctionsContent(chunkFunctions: List<ImageVectorNode.ChunkFunction>?) =
+        if (!chunkFunctions.isNullOrEmpty()) {
+            """|
+               |${chunkFunctions.joinToString("\n\n") { it.createChunkFunction() }}
+               """
+        } else {
+            ""
+        }
+
+    private fun buildIconPropertyName(receiverType: String?) = when {
+        receiverType?.isNotEmpty() == true -> {
+            // as we add the dot in the next line, remove it in case the user adds a leftover dot
+            // to avoid compile issues.
+            receiverType.removeSuffix(".")
+            "$receiverType.${iconName.pascalCase()}"
+        }
+
+        addToMaterial -> "Icons.Filled.${iconName.pascalCase()}"
+        else -> iconName.pascalCase()
+    }
+
+    /**
+     * Builds the preview code for the given icon property name.
+     *
+     * @param iconPropertyName The name of the icon property to generate the preview for.
+     * @return The preview code as a string.
+     */
+    private fun buildPreview(iconPropertyName: String) = if (noPreview) {
+        ""
+    } else {
+        """
+        |
+        |@Preview
+        |@Composable
+        |private fun IconPreview() {
+        |    $theme {
+        |        Column(
+        |            verticalArrangement = Arrangement.spacedBy(8.dp),
+        |            horizontalAlignment = Alignment.CenterHorizontally,
+        |        ) {
+        |            Image(
+        |                imageVector = $iconPropertyName,
+        |                contentDescription = null,
+        |                modifier = Modifier
+        |                    .width((${max(width, viewportWidth)}).dp)
+        |                    .height((${max(height, viewportHeight)}).dp),
+        |            )
+        |        }
+        |    }
+        |}
+        """
+    }
+
+    /**
+     * Builds a string containing the extra content, which includes the chunk
+     * functions and the preview.
+     *
+     * @param chunkFunctionsContent The string representation of the chunk functions.
+     * @param preview The preview string.
+     * @return A string containing the extra content.
+     */
+    private fun buildExtraContent(chunkFunctionsContent: String, preview: String) = buildString {
+        if (chunkFunctionsContent.isNotEmpty()) {
+            appendLine(chunkFunctionsContent.trimMargin())
+        }
+        if (preview.isNotEmpty()) {
+            appendLine(preview.trimMargin())
+        }
+    }
+
+    /**
+     * Checks if the size of the icon exceeds the threshold for a single method
+     * and splits the icon into chunks if needed.
+     *
+     * @return A pair containing the list of nodes and the list of chunk functions
+     * (if any).
+     */
     private fun chunkNodesIfNeeded(): Pair<List<ImageVectorNode>, List<ImageVectorNode.ChunkFunction>?> {
         val byteSize = ICON_BASE_STRUCTURE_BYTE_SIZE + nodes
             .sumOf { it.approximateByteSize }
