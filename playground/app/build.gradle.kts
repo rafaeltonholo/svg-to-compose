eea88ba4c0ef9972d93ef28b7b721efe22cc73e2
Rafael Tonholo <rafael@tonholo.dev>
2024-11-20 09:19:34 -0400

    chore: update playground project to use new plugin api

--- playground/app/build.gradle.kts
+++ playground/app/build.gradle.kts
@@ -2,17 +2,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    id("dev.tonholo.s2c") version "1.0.0-alpha01"
+    id("dev.tonholo.s2c") version "2.0.0-alpha01"
 }
 
 android {
     namespace = "dev.tonholo.svgToCompose.playground"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.tonholo.svgToCompose.playground"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -55,23 +55,25 @@ android {
 
 svgToCompose {
     processor {
+        common {
+            recursive()
+            optimize(enabled = false)
+            icons {
+                theme("dev.tonholo.svgToCompose.playground.ui.theme.SampleAppTheme")
+            }
+        }
         val basePackage = "dev.tonholo.svgToCompose.playground.ui.icon"
-        val theme = "dev.tonholo.svgToCompose.playground.ui.theme.SampleAppTheme"
         create("svg") {
-            origin = layout.projectDirectory.dir("../../samples/svg")
-            destinationPackage = "$basePackage.svg"
-            this.theme = theme
-            recursive = true
-            minified = true
-            optimize = false
+            from(layout.projectDirectory.dir("../../samples/svg"))
+            destinationPackage("$basePackage.svg")
+            icons {
+                optimize(enabled = true)
+                minify()
+            }
         }
         val avg by creating {
-            origin = layout.projectDirectory.dir("../../samples/avg")
-            destinationPackage = "$basePackage.avg"
-            this.theme = theme
-            recursive = true
-            minified = false
-            optimize = true
+            from(layout.projectDirectory.dir("../../samples/avg"))
+            destinationPackage("$basePackage.avg")
         }
     }
 }
