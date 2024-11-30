cfa9bdd5d074f47bc4826c4b031be804686dd2c4
Rafael Tonholo <rafael@tonholo.dev>
2024-11-30 09:52:11 -0400

    refactor: extract SvgRootNode to its own file refactor: viewBox parsing logic, adding memoization and unit tests

--- svg-to-compose/src/commonMain/kotlin/dev/tonholo/s2c/domain/svg/SvgNode.kt
+++ svg-to-compose/src/commonMain/kotlin/dev/tonholo/s2c/domain/svg/SvgNode.kt
@@ -13,8 +13,18 @@ import dev.tonholo.s2c.domain.xml.XmlNode
 import dev.tonholo.s2c.domain.xml.XmlParentNode
 import dev.tonholo.s2c.domain.xml.XmlRootNode
 
+/**
+ * Represents a node in the SVG tree.
+ */
 sealed interface SvgNode : XmlNode {
+    /**
+     * The transform attribute of the node.
+     */
     val transform: SvgTransform?
+
+    /**
+     * Normalizes an id by removing prefixes and suffixes.
+     */
     fun String.normalizedId(): String = with(SvgNode) {
         normalizedId()
     }
@@ -35,6 +45,12 @@ sealed interface SvgNode : XmlNode {
     }
 }
 
+/**
+ * Calculates the stacked transform of the node by traversing up the tree
+ * and concatenating the transform attributes of the ancestors.
+ * @param parent The parent node of the current node.
+ * @return The stacked transform of the node.
+ */
 fun SvgNode.stackedTransform(parent: XmlParentNode): SvgTransform? {
     var stacked = attributes[ATTR_TRANSFORM]
     if (parent !is SvgDefsNode) {
@@ -60,112 +76,13 @@ fun SvgNode.stackedTransform(parent: XmlParentNode): SvgTransform? {
     return stacked?.let(::SvgTransform)
 }
 
-class SvgRootNode(
-    parent: XmlParentNode,
-    override val children: MutableSet<XmlNode>,
-    attributes: MutableMap<String, String>,
-) : SvgElementNode<SvgRootNode>(parent, children, attributes, tagName = TAG_NAME), SvgNode {
-    override val constructor = ::SvgRootNode
-    val width: Float by attribute<SvgLength?, Float> { width ->
-        width?.toFloat(baseDimension = viewportWidth)
-            ?: viewportWidth
-    }
-    val height: Float by attribute<SvgLength?, Float> { height ->
-        height?.toFloat(baseDimension = viewportHeight)
-            ?: viewportHeight
-    }
-    var viewBox: FloatArray by attribute<String?, _> { viewBox ->
-        parseViewBox(viewBox) ?: floatArrayOf(0f, 0f, width, height)
-    }
-    var fill: String? by attribute()
-
-    private val viewportX: Float by lazy {
-        getDimensionFromViewBox(SVG_VIEW_BOX_X_POSITION) ?: 0f
-    }
-
-    private val viewportY: Float by lazy {
-        getDimensionFromViewBox(SVG_VIEW_BOX_Y_POSITION) ?: 0f
-    }
-
-    val viewportWidth: Float by lazy {
-        getDimensionFromViewBox(SVG_VIEW_BOX_WIDTH_POSITION) ?: safeWidth ?: SVG_DEFAULT_WIDTH
-    }
-
-    val viewportHeight: Float by lazy {
-        getDimensionFromViewBox(SVG_VIEW_BOX_HEIGHT_POSITION) ?: safeHeight ?: SVG_DEFAULT_HEIGHT
-    }
-
-    val defs: HashMap<String, SvgUseNode> = hashMapOf()
-    val gradients: HashMap<String, SvgGradient<*>> = hashMapOf()
-
-    override val transform: SvgTransform? by attribute<String?, SvgTransform?> {
-        var transform = it
-        if (viewportX != 0f || viewportY != 0f) {
-            transform = "translate(${-viewportX}, ${-viewportY})"
-            attributes[ATTR_TRANSFORM] = transform
-        }
-        transform?.let(::SvgTransform)
-    }
-
-    /**
-     * Checks if width is present in the attribute map.
-     * If it is the case, return the [width] property which
-     * calculates the correct width based on a [SvgLength],
-     * otherwise null.
-     *
-     * This is required since both width and viewBox attributes
-     * can be omitted.
-     */
-    private val safeWidth: Float?
-        inline get() = attributes[ATTR_WIDTH]
-            ?.let(::SvgLength)
-            ?.toFloat(baseDimension = SVG_DEFAULT_WIDTH)
-
-    /**
-     * Checks if width is present in the attribute map.
-     * If it is the case, return the [height] property which
-     * calculates the correct height based on a [SvgLength],
-     * otherwise null.
-     *
-     * This is required since both height and viewBox attributes
-     * can be omitted.
-     */
-    private val safeHeight: Float?
-        inline get() = attributes[ATTR_HEIGHT]
-            ?.let(::SvgLength)
-            ?.toFloat(baseDimension = SVG_DEFAULT_HEIGHT)
-
-    private inline fun parseViewBox(viewBox: String?): FloatArray? =
-        viewBox?.split(", ", ",", " ")?.map { it.toFloat() }?.toFloatArray()
-
-    private inline fun getDimensionFromViewBox(dimensionIndex: Int): Float? =
-        parseViewBox(attributes[ATTR_VIEW_BOX])?.getOrNull(dimensionIndex)
-
-    companion object {
-        const val TAG_NAME = "svg"
-
-        /**
-         * The default value if both width and viewBox are omitted.
-         * @see <a href="https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/width#svg">
-         *          Width attribute on SVG
-         *      </a>
-         */
-        private const val SVG_DEFAULT_WIDTH = 300f
-
-        /**
-         * The default value if both height and viewBox are omitted.
-         * @see <a href="https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/height#svg">
-         *          Height attribute on SVG
-         *     </a>
-         */
-        private const val SVG_DEFAULT_HEIGHT = 150f
-        const val SVG_VIEW_BOX_X_POSITION = 0
-        const val SVG_VIEW_BOX_Y_POSITION = 1
-        const val SVG_VIEW_BOX_WIDTH_POSITION = 2
-        const val SVG_VIEW_BOX_HEIGHT_POSITION = 3
-    }
-}
-
+/**
+ * Converts the SVG node to a list of [ImageVectorNode].
+ * @param masks The list of masks defined in the SVG.
+ * @param minified Whether to minify the output.
+ * @return A list of [ImageVectorNode] representing the SVG node.
+ * Returns `null` if the node is not supported.
+ */
 fun SvgNode.asNodes(
     masks: List<SvgMaskNode>,
     minified: Boolean,
