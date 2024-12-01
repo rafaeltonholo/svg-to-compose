9939861429eb1a46844ce1bb8eb1d2e525c5de20
Rafael Tonholo <rafael@tonholo.dev>
2024-12-01 13:03:26 -0400

    feat(lexer): enhance css lexer to support dimensions and percentages
    This commit enhances the CSS lexer to support dimensions (e.g., px, em, rem) and percentages.
    
    The lexer can now recognize and tokenize dimensions and percentages, which are crucial for handling CSS properties related to sizes, lengths, and proportions.
    
    This enhancement improves the lexer's ability to parse CSS rules and extract more comprehensive information about styles.

--- svg-to-compose/src/commonTest/kotlin/dev/tonholo/s2c/lexer/css/CssLexerTest.kt
+++ svg-to-compose/src/commonTest/kotlin/dev/tonholo/s2c/lexer/css/CssLexerTest.kt
@@ -129,8 +129,7 @@ class CssLexerTest {
             Token(kind = CssTokenKind.Identifier, startOffset = 90, endOffset = 103),
             Token(kind = CssTokenKind.Colon, startOffset = 103, endOffset = 104),
             Token(kind = CssTokenKind.WhiteSpace, startOffset = 104, endOffset = 105),
-            Token(kind = CssTokenKind.Number, startOffset = 105, endOffset = 106),
-            Token(kind = CssTokenKind.Identifier, startOffset = 106, endOffset = 108),
+            Token(kind = CssTokenKind.Dimension, startOffset = 105, endOffset = 108),
             Token(kind = CssTokenKind.Semicolon, startOffset = 108, endOffset = 109),
             Token(kind = CssTokenKind.WhiteSpace, startOffset = 109, endOffset = 110),
             Token(kind = CssTokenKind.CloseCurlyBrace, startOffset = 110, endOffset = 111),
@@ -311,7 +310,7 @@ class CssLexerTest {
     }
 
     @Test
-    fun `create token for css rule with aggregate selectors using identifiers and class`() {
+    fun `create tokens for css rule with aggregate selectors using identifiers and class`() {
         val content = """
             |div span label .my-class {
             |    color: blue;
@@ -337,6 +336,139 @@ class CssLexerTest {
             Token(kind = CssTokenKind.WhiteSpace, startOffset = 43, endOffset = 44),  // '\n'
             Token(kind = CssTokenKind.CloseCurlyBrace, startOffset = 44, endOffset = 45), // '}'
             Token(kind = CssTokenKind.EndOfFile, startOffset = 45, endOffset = 45),
+        )
+        assert(content, tokens)
+    }
+
+    @Test
+    fun `create tokens for complex css rule`() {
+        val content = """
+            |@media screen and (min-width: 768px) {
+            |    body.homepage .main-content > section:first-of-type:not(.hidden) {
+            |        display: flex;
+            |        flex-direction: column;
+            |        align-items: center;
+            |        background-color: rgba(255, 255, 255, 0.8);
+            |        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            |    }
+            |}
+        """.trimMargin()
+        val tokens = listOf(
+            // @media screen and (min-width: 768px) {
+            Token(kind = CssTokenKind.AtKeyword, startOffset = 0, endOffset = 6),            // '@media'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 6, endOffset = 7),           // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 7, endOffset = 13),          // 'screen'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 13, endOffset = 14),         // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 14, endOffset = 17),         // 'and'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 17, endOffset = 18),         // ' '
+            Token(kind = CssTokenKind.OpenParenthesis, startOffset = 18, endOffset = 19),    // '('
+            Token(kind = CssTokenKind.Identifier, startOffset = 19, endOffset = 28),         // 'min-width'
+            Token(kind = CssTokenKind.Colon, startOffset = 28, endOffset = 29),              // ':'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 29, endOffset = 30),         // ' '
+            Token(kind = CssTokenKind.Dimension, startOffset = 30, endOffset = 35),          // '768px'
+            Token(kind = CssTokenKind.CloseParenthesis, startOffset = 35, endOffset = 36),   // ')'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 36, endOffset = 37),         // ' '
+            Token(kind = CssTokenKind.OpenCurlyBrace, startOffset = 37, endOffset = 38),     // '{'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 38, endOffset = 43),         // '\n    '
+
+            // body.homepage .main-content > section:first-of-type:not(.hidden) {
+            Token(kind = CssTokenKind.Identifier, startOffset = 43, endOffset = 47),         // 'body'
+            Token(kind = CssTokenKind.Dot, startOffset = 47, endOffset = 48),                // '.'
+            Token(kind = CssTokenKind.Identifier, startOffset = 48, endOffset = 56),         // 'homepage'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 56, endOffset = 57),         // ' '
+            Token(kind = CssTokenKind.Dot, startOffset = 57, endOffset = 58),                // '.'
+            Token(kind = CssTokenKind.Identifier, startOffset = 58, endOffset = 70),         // 'main-content'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 70, endOffset = 71),         // ' '
+            Token(kind = CssTokenKind.Greater, startOffset = 71, endOffset = 72),        // '>'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 72, endOffset = 73),         // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 73, endOffset = 80),         // 'section'
+            Token(kind = CssTokenKind.Colon, startOffset = 80, endOffset = 81),              // ':'
+            Token(kind = CssTokenKind.Identifier, startOffset = 81, endOffset = 94),         // 'first-of-type'
+            Token(kind = CssTokenKind.Colon, startOffset = 94, endOffset = 95),              // ':'
+            Token(kind = CssTokenKind.Identifier, startOffset = 95, endOffset = 98),         // 'not'
+            Token(kind = CssTokenKind.OpenParenthesis, startOffset = 98, endOffset = 99),    // '('
+            Token(kind = CssTokenKind.Dot, startOffset = 99, endOffset = 100),               // '.'
+            Token(kind = CssTokenKind.Identifier, startOffset = 100, endOffset = 106),       // 'hidden'
+            Token(kind = CssTokenKind.CloseParenthesis, startOffset = 106, endOffset = 107), // ')'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 107, endOffset = 108),       // ' '
+            Token(kind = CssTokenKind.OpenCurlyBrace, startOffset = 108, endOffset = 109),   // '{'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 109, endOffset = 118),       // '\n        '
+
+            // Declaration: display: flex;
+            Token(kind = CssTokenKind.Identifier, startOffset = 118, endOffset = 125),       // 'display'
+            Token(kind = CssTokenKind.Colon, startOffset = 125, endOffset = 126),            // ':'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 126, endOffset = 127),       // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 127, endOffset = 131),       // 'flex'
+            Token(kind = CssTokenKind.Semicolon, startOffset = 131, endOffset = 132),        // ';'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 132, endOffset = 141),       // '\n        '
+
+            /* Declaration: flex-direction: column; */
+            Token(kind = CssTokenKind.Identifier, startOffset = 141, endOffset = 155),       // 'flex-direction'
+            Token(kind = CssTokenKind.Colon, startOffset = 155, endOffset = 156),            // ':'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 156, endOffset = 157),       // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 157, endOffset = 163),       // 'column'
+            Token(kind = CssTokenKind.Semicolon, startOffset = 163, endOffset = 164),        // ';'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 164, endOffset = 173),       // '\n        '
+
+            /* Declaration: align-items: center; */
+            Token(kind = CssTokenKind.Identifier, startOffset = 173, endOffset = 184),       // 'align-items'
+            Token(kind = CssTokenKind.Colon, startOffset = 184, endOffset = 185),            // ':'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 185, endOffset = 186),       // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 186, endOffset = 192),       // 'center'
+            Token(kind = CssTokenKind.Semicolon, startOffset = 192, endOffset = 193),        // ';'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 193, endOffset = 202),       // '\n        '
+
+            /* Declaration: background-color: rgba(255, 255, 255, 0.8); */
+            Token(kind = CssTokenKind.Identifier, startOffset = 202, endOffset = 218),       // 'background-color'
+            Token(kind = CssTokenKind.Colon, startOffset = 218, endOffset = 219),            // ':'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 219, endOffset = 220),       // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 220, endOffset = 224),       // 'rgba'
+            Token(kind = CssTokenKind.OpenParenthesis, startOffset = 224, endOffset = 225),  // '('
+            Token(kind = CssTokenKind.Number, startOffset = 225, endOffset = 228),           // '255'
+            Token(kind = CssTokenKind.Comma, startOffset = 228, endOffset = 229),            // ','
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 229, endOffset = 230),       // ' '
+            Token(kind = CssTokenKind.Number, startOffset = 230, endOffset = 233),           // '255'
+            Token(kind = CssTokenKind.Comma, startOffset = 233, endOffset = 234),            // ','
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 234, endOffset = 235),       // ' '
+            Token(kind = CssTokenKind.Number, startOffset = 235, endOffset = 238),           // '255'
+            Token(kind = CssTokenKind.Comma, startOffset = 238, endOffset = 239),            // ','
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 239, endOffset = 240),       // ' '
+            Token(kind = CssTokenKind.Number, startOffset = 240, endOffset = 243),           // '0.8'
+            Token(kind = CssTokenKind.CloseParenthesis, startOffset = 243, endOffset = 244), // ')'
+            Token(kind = CssTokenKind.Semicolon, startOffset = 244, endOffset = 245),        // ';'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 245, endOffset = 254),       // '\n        '
+
+            /* Declaration: box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); */
+            Token(kind = CssTokenKind.Identifier, startOffset = 254, endOffset = 264),       // 'box-shadow'
+            Token(kind = CssTokenKind.Colon, startOffset = 264, endOffset = 265),            // ':'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 265, endOffset = 266),       // ' '
+            Token(kind = CssTokenKind.Number, startOffset = 266, endOffset = 267),           // '0'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 267, endOffset = 268),       // ' '
+            Token(kind = CssTokenKind.Dimension, startOffset = 268, endOffset = 271),        // '4px'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 271, endOffset = 272),       // ' '
+            Token(kind = CssTokenKind.Dimension, startOffset = 272, endOffset = 275),        // '6px'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 275, endOffset = 276),       // ' '
+            Token(kind = CssTokenKind.Identifier, startOffset = 276, endOffset = 280),       // 'rgba'
+            Token(kind = CssTokenKind.OpenParenthesis, startOffset = 280, endOffset = 281),  // '('
+            Token(kind = CssTokenKind.Number, startOffset = 281, endOffset = 282),           // '0'
+            Token(kind = CssTokenKind.Comma, startOffset = 282, endOffset = 283),            // ','
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 283, endOffset = 284),       // ' '
+            Token(kind = CssTokenKind.Number, startOffset = 284, endOffset = 285),           // '0'
+            Token(kind = CssTokenKind.Comma, startOffset = 285, endOffset = 286),            // ','
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 286, endOffset = 287),       // ' '
+            Token(kind = CssTokenKind.Number, startOffset = 287, endOffset = 288),           // '0'
+            Token(kind = CssTokenKind.Comma, startOffset = 288, endOffset = 289),            // ','
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 289, endOffset = 290),       // ' '
+            Token(kind = CssTokenKind.Number, startOffset = 290, endOffset = 293),           // '0.1'
+            Token(kind = CssTokenKind.CloseParenthesis, startOffset = 293, endOffset = 294), // ')'
+            Token(kind = CssTokenKind.Semicolon, startOffset = 294, endOffset = 295),        // ';'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 295, endOffset = 300),       // '\n        '
+
+            /* Closing curly braces */
+            Token(kind = CssTokenKind.CloseCurlyBrace, startOffset = 300, endOffset = 301),  // '}'
+            Token(kind = CssTokenKind.WhiteSpace, startOffset = 301, endOffset = 302),       // '\n'
+            Token(kind = CssTokenKind.CloseCurlyBrace, startOffset = 302, endOffset = 303),  // '}'
+            Token(kind = CssTokenKind.EndOfFile, startOffset = 303, endOffset = 303),
         )
         assert(content, tokens)
     }
