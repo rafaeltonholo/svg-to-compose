7ae3d0387bf8b17d13f2b77089db9a4cfa46064b
Rafael Tonholo <rafael@tonholo.dev>
2024-12-11 19:05:55 -0400

    refactor: improve string extensions and remove not used functions feat: add `String.prependIndent` function deprecating `String.indented` docs: add missing function ktdocs

--- svg-to-compose/src/commonMain/kotlin/dev/tonholo/s2c/extensions/String.extension.kt
+++ svg-to-compose/src/commonMain/kotlin/dev/tonholo/s2c/extensions/String.extension.kt
@@ -5,18 +5,44 @@ import kotlin.math.roundToInt
 
 private const val PERCENT = 100f
 
+/**
+ * Replaces dividers in the string with empty strings and capitalizes the following letter.
+ *
+ * Dividers are considered to be any of the following characters: '_', '-', '.', ' '.
+ *
+ * For example:
+ * "my_string" becomes "myString"
+ * "my-string" becomes "myString"
+ * "my.string" becomes "myString"
+ * "my string" becomes "myString"
+ */
 private fun String.replaceDividers(): String {
     val pattern = "([_\\-. ])[a-zA-Z0-9]".toRegex()
     return replace(pattern) { it.value.last().uppercase() }
 }
 
+/**
+ * Converts a string to camel case.
+ */
 fun String.camelCase(): String = replaceDividers()
     .replaceFirstChar { it.lowercaseChar() }
 
+/**
+ * Converts a string to pascal case.
+ */
 fun String.pascalCase(): String = replaceDividers()
     .replaceFirstChar { it.uppercaseChar() }
 
+@Deprecated(
+    message = "Use prependIndent instead",
+    replaceWith = ReplaceWith(expression = "this.prependIndent(indentSize)"),
+)
 fun String.indented(indentSize: Int) = " ".repeat(indentSize) + this
+
+/**
+ * Prepends an indent to the string.
+ */
+fun String.prependIndent(indentSize: Int) = prependIndent(" ".repeat(indentSize))
 
 /**
  * Some properties on SVG can receive a Number or a percentage.
@@ -33,6 +59,26 @@ fun String.toLengthFloat(
     height: Float,
 ): Float = toLengthFloatOrNull(width, height) ?: throw NumberFormatException("Can't parse '$this' to Float")
 
+/**
+ * Converts a string representation of a length to a float value.
+ *
+ * The input string can be either a percentage value (e.g., "50%") or a
+ * plain float value (e.g., "10.5").
+ *
+ * If the input string is a percentage value, the result is calculated as
+ * a percentage of the maximum between the given width and height.
+ *
+ * For example, if the input is "50%" and the width is 100 and the height is 200,
+ * the result will be 100 (50% of 200).
+ *
+ * If the input string is a plain float value, the result is the float value parsed from the string.
+ *
+ * If the input string cannot be parsed to a float value, the result is null.
+ *
+ * @param width The width to use for percentage calculations.
+ * @param height The height to use for percentage calculations.
+ * @return The float value represented by the input string, or null if the string cannot be parsed.
+ */
 fun String.toLengthFloatOrNull(
     width: Float,
     height: Float,
@@ -42,15 +88,9 @@ fun String.toLengthFloatOrNull(
     this.toFloatOrNull()
 }
 
-fun String.toLengthInt(
-    width: Int,
-    height: Int = width,
-): Int = toLengthFloat(width.toFloat(), height.toFloat()).roundToInt()
-
-fun String.toLengthInt(
-    height: Int,
-): Int = toLengthFloat(height.toFloat(), height.toFloat()).roundToInt()
-
+/**
+ * Converts a string to a percentage float value.
+ */
 fun String.toPercentage(): Float {
     val value = this.toFloatOrNull()
     return when {
@@ -60,4 +100,7 @@ fun String.toPercentage(): Float {
     }.coerceIn(range = 0f..1f)
 }
 
+/**
+ * Removes trailing zeros from a string representing a number.
+ */
 inline fun String.removeTrailingZero(): String = replace("\\.0\\b".toRegex(), "")
