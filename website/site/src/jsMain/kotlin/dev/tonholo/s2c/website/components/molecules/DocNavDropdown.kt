3cfc801d7f33dc1dbd7120ff2e1a275b9c52fd85
Rafael Tonholo <rafael@tonholo.dev>
2026-03-23 11:25:33 -0300

    fix(website): update DocNavDropdown and InlineCode imports

--- website/site/src/jsMain/kotlin/dev/tonholo/s2c/website/components/molecules/DocNavDropdown.kt
+++ website/site/src/jsMain/kotlin/dev/tonholo/s2c/website/components/molecules/DocNavDropdown.kt
@@ -45,7 +45,7 @@ import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.style.CssStyle
 import com.varabyte.kobweb.silk.style.toModifier
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
-import dev.tonholo.s2c.website.toSitePalette
+import dev.tonholo.s2c.website.theme.toSitePalette
 import org.jetbrains.compose.web.css.DisplayStyle
 import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.Position
