27339bed72c85d9a8cb0d41a822670e44ba630bf
Rafael Tonholo <rafael@tonholo.dev>
2026-04-14 12:20:19 -0300

    refactor(worker): extract shared bindings into SvgToComposeBindings interface
    This is required since we need to remove the Contribution Hints from Metro for JS targes

--- website/worker/src/jsMain/kotlin/dev/tonholo/s2c/website/worker/inject/WorkerGraph.kt
+++ website/worker/src/jsMain/kotlin/dev/tonholo/s2c/website/worker/inject/WorkerGraph.kt
@@ -2,11 +2,9 @@ package dev.tonholo.s2c.website.worker.inject
 
 import dev.tonholo.s2c.Converter
 import dev.tonholo.s2c.DefaultConverter
-import dev.tonholo.s2c.domain.FileType
+import dev.tonholo.s2c.emitter.CodeEmitterFactory
+import dev.tonholo.s2c.inject.SvgToComposeBindings
 import dev.tonholo.s2c.logger.Logger
-import dev.tonholo.s2c.parser.AvgContentParser
-import dev.tonholo.s2c.parser.ContentParser
-import dev.tonholo.s2c.parser.SvgContentParser
 import dev.tonholo.s2c.website.worker.ConsoleLogger
 import dev.zacsweers.metro.Binds
 import dev.zacsweers.metro.DependencyGraph
@@ -15,7 +13,7 @@ import kotlinx.serialization.json.Json
 
 /** Metro dependency graph providing [Converter], [CodeEmitterFactory], and [Json] instances for the worker. */
 @DependencyGraph
-internal interface WorkerGraph {
+internal interface WorkerGraph : SvgToComposeBindings {
     val converter: Converter
     val json: Json
 
@@ -28,13 +26,6 @@ internal interface WorkerGraph {
     @Provides
     @EnableDebugQualifier
     val enableDebug: Boolean get() = false
-
-    @Provides
-    fun provideContentParsers(svgParser: SvgContentParser, avgParser: AvgContentParser): Map<FileType, ContentParser> =
-        mapOf(
-            FileType.Svg to svgParser,
-            FileType.Avg to avgParser,
-        )
 
     @Provides
     fun provideJson(): Json = Json { ignoreUnknownKeys = true }
