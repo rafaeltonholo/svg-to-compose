85c6bb1366c2109069816a6f3e952da9930222fe
Rafael Tonholo <rafael@tonholo.dev>
2026-04-15 15:28:11 -0300

    chore(cli): remove unused Deferred import

--- modules/cli/src/commonMain/kotlin/dev/tonholo/s2c/cli/runtime/CliRunner.kt
+++ modules/cli/src/commonMain/kotlin/dev/tonholo/s2c/cli/runtime/CliRunner.kt
@@ -26,7 +26,6 @@ import dev.zacsweers.metro.Inject
 import okio.Path
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
